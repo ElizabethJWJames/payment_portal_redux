1c3f05fbc7e5e1796353f2f0f840996877819ed4
ElizabethJWJames <EJames@autostart.com>
2018-02-14 16:02:03 -0500

    Updates

--- src/redux-auto/example/store/posts/delete.js
+++
@@ -1,5 +0,0 @@
-
-export default function (posts, {id}) {
-  console.log("posts / delete >> delete post with id",id)
-  return posts.filter(post => post.id !== id);
-}
--- src/redux-auto/example/store/posts/index.js
+++
@@ -1,9 +0,0 @@
-
-import actions from 'redux-auto'
-
-export default function posts(posts = [], action) {
-  console.log("posts / index >> default function for posts | TYPE = ",action.type)
-  return posts;
-}
-
-
